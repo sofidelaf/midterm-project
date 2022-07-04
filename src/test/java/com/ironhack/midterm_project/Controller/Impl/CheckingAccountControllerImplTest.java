5b4ff30270efb3bafeb43d9b707307562a152e39
la Fuente Maschi <s.la.fuente.maschi@accenture.com>
2022-07-04 18:29:16 +0200

    Accounts tests

---
+++ src/test/java/com/ironhack/midterm_project/Controller/Impl/CheckingAccountControllerImplTest.java
@@ -0,0 +1,30 @@
+package com.ironhack.midterm_project.Controller.Impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingAccountControllerImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void getPersonalAccountInformation() {
+    }
+}
---
+++ src/test/java/com/ironhack/midterm_project/Controller/Impl/StudentCheckingAccountControllerImplTest.java
@@ -0,0 +1,26 @@
+package com.ironhack.midterm_project.Controller.Impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentCheckingAccountControllerImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getPersonalAccountInformation() {
+    }
+}
